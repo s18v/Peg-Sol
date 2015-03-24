87038c09db58a2adb6ff6dc049dabcc830c64116
Saran <sarankumarv@users.noreply.github.com>
2015-03-24 16:23:12 -0700

    Update utils.js
    Generating the board

--- js/utils.js
+++ js/utils.js
@@ -7,14 +7,43 @@ var board = document.getElementById("board");
 var context = board.getContext("2d");
 
 function generateSquare(x, y, color) {
-    context.fillStyle = "rgb(0,0,0)"; //Black - #000000
-    context.fillRect(x * boxWidth,y*boxWidth,boxWidth,boxWidth); //(x-coordinate from upper-left, y-coordinate, width, height)
-   
-    // remove these
-    context.fillRect(60,0,30,15);
-    context.fillRect(120,0,30,15);  
+   context.fillStyle = "rgb(255,255,255)"; //White - #FFFFFF
+    context.fillRect(x * boxWidth, y * boxWidth, boxWidth, boxWidth); //(x-coordinate from upper-left, y-coordinate, width, height)
+    context.fillStyle = squareColor;
+    context.fillRect((x * boxWidth) + offset, (y * boxWidth) + offset, boxWidth - 2 * offset, boxWidth - 2 * offset);
 }
+
+function generateCircle(x, y, color) {
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x * boxWidth + (boxWidth / 2), y * boxWidth + (boxWidth / 2), boxWidth * 0.4, 2 * Math.PI, true);
+    context.closePath();
+    context.fill();
+}
+
 function start() {
-     generateSquare(2, 0, squareColor);
-     generateSquare(2, 1, squareColor);
+    //    generateSquare(2, 0, squareColor);
+    
+        board.width = 7 * boxWidth;
+        board.height = 7 * boxWidth
+        for (var i=0; i < 3; i++) {
+			for (var j=0; j < 2; j++) {
+				generateSquare(2+i, j, squareColor);
+//				generateCircle(2+i, j, circleColor);
+            }
+        }
+		for (var i=0; i < 7; i++) {
+			for (var j=0; j < 3; j++) {
+				generateSquare(i, 2+j, squareColor);
+				if (j != 1 || i != 3) {
+//					drawDot(i, 2+j, PEG_COLOUR);
+                }
+            }
+        }
+        for (var i=0; i < 3; i++) {
+			for (var j=0; j < 2; j++) {
+				generateSquare(2+i, 5+j, squareColor);
+//				drawDot(2+i, 5+j, PEG_COLOUR);
+			}
+        }
 }
