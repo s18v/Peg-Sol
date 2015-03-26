1664280bbe10fd70f2e37dd1124a0ef0374eb992
Saran <saranvellanki@Sarans-Macintosh.local>
2015-03-25 23:40:13 -0700

    Updating script.js
    Adding functionality to find the position on clicking the board

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
-var temp;
+/**
+* This method is to find the relative position of the element from its parent.
+* Using JQuery's offset() instead of this.
+*/
+function findPosition(obj) {
+    var curLeft = 0;
+    var curTop = 0;
+    if (obj.offsetParent) {
+        do {
+            curLeft += obj.offsetLeft;
+            curTop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+        return { x: curLeft, y: curTop};
+    }
+    return undefined;
+}
+
+$("#board").click(function(event) {
+    console.log("X: " + event.pageX + " and Y: " + event.pageY); 
+    console.log(this.offsetParent); //offsetParent returns a reference to the object which is the closest (nearest in the containment hierarchy) positioned containing element
+   
+    /* var pos = findPosition(this);
+    var context = this.getContext('2d');
+    var x = event.pageX - pos.x; //event.pageX gives the position relative to the document's left
+    var y = event.pageY - pos.y; //event.pageY gives the position relative to the document's top
+    console.log("relative X: " + x + " and relative Y: " + y); */
+    
+    console.log("checking jQuery's results...");
+    var xjquery = event.pageX - $(this).offset().left;
+    var yjquery = event.pageY - $(this).offset().top;
+    
+    console.log("jquery X: " + xjquery + " and jquery Y: " + yjquery);
+    console.log("......");
+
+});
