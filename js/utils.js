c0c3e0dd85b417f0aaa33158666d2372df5110e3
Saran <saranvellanki@Sarans-Macintosh.local>
2015-03-25 21:57:52 -0700

    Adding scripts

--- js/utils.js
+++ js/utils.js
@@ -22,27 +22,26 @@ function generateCircle(x, y, color) {
 }
 
 function start() {
-    
-        board.width = 7 * boxWidth;
-        board.height = 7 * boxWidth
-        for (var i=0; i < 3; i++) {
-			for (var j=0; j < 2; j++) {
-				generateSquare(2+i, j, squareColor);
-				generateCircle(2+i, j, circleColor);
+    board.width = 7 * boxWidth;
+    board.height = 7 * boxWidth;
+    for (var i=0; i < 3; i++) {
+        for (var j=0; j < 2; j++) {
+            generateSquare(2+i, j, squareColor);
+            generateCircle(2+i, j, pegColor);
+        }
+    }
+    for (var i=0; i < 7; i++) {
+        for (var j=0; j < 3; j++) {
+            generateSquare(i, 2+j, squareColor);
+            if (j != 1 || i != 3) {
+                generateCircle(i, 2+j, pegColor);
             }
         }
-	for (var i=0; i < 7; i++) {
-		for (var j=0; j < 3; j++) {
-			generateSquare(i, 2+j, squareColor);
-			if (j != 1 || i != 3) {
-				generateCircle(i, 2+j, circleColor);
-               		}
-               	}
+    }
+    for (var i=0; i < 3; i++) {
+        for (var j=0; j < 2; j++) {
+            generateSquare(2+i, 5+j, squareColor);
+            generateCircle(2+i, 5+j, pegColor);
         }
-        for (var i=0; i < 3; i++) {
-			for (var j=0; j < 2; j++) {
-				generateSquare(2+i, 5+j, squareColor);
-				generateCircle(2+i, 5+j, circleColor);
-			}
-        }
+    }
 }
